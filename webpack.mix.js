f335014e73f5d48534ae282e149dbf6574ec35cf
Masuda Ryosuke <m1741508@gmail.com>
2025-04-03 23:37:11 +0900

    初期設定完了

---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .babelConfig({
+        presets: [
+            ['@babel/preset-env', {
+                targets: '> 0.25%, not dead',  // 対応ブラウザの指定
+            }]
+        ]
+    });
