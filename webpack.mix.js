c4483ba9934c6888a97b5f9bf8ecae645ea233da
Masuda Ryosuke <m1741508@gmail.com>
2025-06-02 20:36:22 +0900

    no message

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,7 @@ mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css', {
         sassOptions:{
-            url:false
+            url: false
         }
     })
     .babelConfig({
